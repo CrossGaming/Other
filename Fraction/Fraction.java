3d61c2740d4c71923770f5be90afe01e16adbbcf
Chris Wright <chris.modchris@yahoo.com>
2013-01-29 21:41:40 -0600

    Finished most methods
    All but LCD, LCM, GCD

--- Fraction/Fraction.java
+++ Fraction/Fraction.java
@@ -43,44 +43,56 @@ public class Fraction
 	{
 		return this.numerator/this.denominator;
 	}
-	public Fraction add(int a)
+	public int getLCD(Fraction f)//Finds the least common denominator
 	{
 		
+	}
+	public static int getLCM(int a, int b)//Finds the least common multiple, for finding common denominators
+	{
+		
+	}
+	public static int getGCD(int a, int b)//Finds the greatest common devisor, for simplifying purposes
+	{
+		
+	}
+	public Fraction add(int a)
+	{
+		return new Fraction(this.numerator+(this.denominator*a),this.denominator);
 	}
 	public Fraction add(Fraction f)
 	{
-		
+		return new Fraction(this.numerator*(this.getLCD(f)/this.denominator)+(f.numerator*(this.getLCD(f)/f.denominator)),this.getLCD(f));
 	}
 	public Fraction subtract(int s)
 	{
-		
+		return new Fraction(this.numerator-(this.denominator*s),this.denominator);
 	}
-	public Fraction subtract(Fraction s)
+	public Fraction subtract(Fraction f)
 	{
-		
+		return new Fraction(this.numerator*(this.getLCD(f)/this.denominator)-(f.numerator*(this.getLCD(f)/f.denominator)),this.getLCD(f));
 	}
 	public Fraction multiply(int m)
 	{
-		
+		return new Fraction(this.numerator*m,this.denominator);
 	}
 	public Fraction multiply(Fraction m)
 	{
-		
+		return new Fraction(this.numerator*m.numerator,this.denominator*m.denominator);
 	}
 	public Fraction divide(int d)
 	{
-		
+		return new Fraction(this.numerator/d, this.denominator);
 	}
 	public Fraction divide(Fraction d)
 	{
-		
+		return new Fraction(this.numerator*d.denominator, this.denominator*d.numerator);
 	}
 	public Fraction simplify()
 	{
-		
+		return new Fraction(this.numerator/Fraction.getGCD(this.numerator, this.denominator),this.denominator/Fraction.getGCD(this.numerator, this.denominator));
 	}
-	public double valueOf(Fraction f)
+	public double findValue()
 	{
-		return f.numerator/f.denominator;
+		return this.numerator/this.denominator;
 	}
 }
